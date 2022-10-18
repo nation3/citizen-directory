6a9780b773f3f9d937ecfc0a982ffda139a22744
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2022-10-18 10:10:23 +0800

    feat: add citizen directory to sidebar

--- components/Nation3Wrapper.tsx
+++ components/Nation3Wrapper.tsx
@@ -1,6 +1,7 @@
-import { Squares2X2Icon } from "@heroicons/react/24/outline";
+import { CurrencyDollarIcon, HomeIcon, LockClosedIcon, NewspaperIcon, PlusIcon, Squares2X2Icon, UserPlusIcon, UsersIcon } from "@heroicons/react/24/outline";
 import { Nation3App, DefaultLayout, DefaultSidebar } from "@nation3/components";
 import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { appDomain, balancerDomain, etherscanDomain, nationToken } from '../lib/config'
 
 function Nation3Wrapper({
   children,
@@ -25,11 +26,46 @@ function Nation3Wrapper({
             onRoute={console.log}
             navLinks={[
               {
-                route: "/",
+                route: `${appDomain}`,
                 icon: <Squares2X2Icon className="w-5 h-5" />,
-                name: "Start",
-                isActive: true,
+                name: "Start"
               },
+              {
+                route: '/',
+                name: 'Citizen directory',
+                icon: <UsersIcon className="h-5 w-5" />,
+                isActive: true
+              },
+              {
+                route: `${appDomain}/join`,
+                name: 'Become a citizen',
+                icon: <UserPlusIcon className="h-5 w-5" />
+              },
+              {
+                route: `${appDomain}/lock`,
+                name: 'Lock tokens',
+                icon: <LockClosedIcon className="h-5 w-5" />
+              },
+              {
+                route: `${appDomain}/liquidity`,
+                name: 'Liquidity rewards',
+                icon: <CurrencyDollarIcon className="h-5 w-5" />
+              },
+              {
+                route: `${balancerDomain}/#/trade/ether/${nationToken}`,
+                name: 'Buy $NATION',
+                icon: <PlusIcon className="h-5 w-5" />
+              },
+              {
+                route: 'https://nation3.org',
+                name: 'Homepage',
+                icon: <HomeIcon className="h-5 w-5" />
+              },
+              {
+                route: 'https://wiki.nation3.org',
+                name: 'Wiki',
+                icon: <NewspaperIcon className="h-5 w-5" />
+              }
             ]}
             connectors={connectors.map((connector) => ({
               ...connector,
