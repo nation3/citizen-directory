3be46e58eb0028049c558d6790555c5622466bc7
Tony Stark <afemiadom@icloud.com>
2023-02-16 16:55:01 -0600

    Removed previous Nation3 Wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,12 @@
-import { getDefaultProvider } from 'ethers'
 import type { AppProps } from 'next/app'
-import { createClient, WagmiConfig } from 'wagmi'
-import Nation3Wrapper from '../components/Nation3Wrapper'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
-const client = createClient({
-  autoConnect: true,
-  provider: getDefaultProvider(),
-})
-
 function Nation3({ Component, pageProps }: AppProps) {
   return (
-    <WagmiConfig client={client}>
-      {/* <Nation3Wrapper> */}
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-      {/* </Nation3Wrapper> */}
-    </WagmiConfig>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   )
 }
 
