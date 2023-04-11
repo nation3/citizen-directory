edf66ba4839d5784cf64a8483e3c0511eda0c108
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2023-04-11 20:15:44 +0800

    refactor: dark color scheme (#14)

--- responsive/src/pages/index.tsx
+++ responsive/src/pages/index.tsx
@@ -3,11 +3,18 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='flex'>
-      <div className='w-80 h-screen bg-white p-8'>
+      <div id='navBar' className='w-80 h-screen p-8'>
         Navbar
       </div>
+
       <div className='p-8'>
-        Content
+        <h1 className="text-3xl font-medium">
+          Nation3 Citizens
+        </h1>
+
+        <div className='mt-4'>
+          Content
+        </div>
       </div>
     </main>
   )
